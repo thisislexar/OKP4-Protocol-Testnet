6841ea2ad1d04b46c6e07797ca4f07756aa01993
Lexar <burakinkaya@sabanciuniv.edu>
2022-10-18 23:46:33 +0300

    Create okp4.sh

---
+++ okp4.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+echo -e "\033[0;35m"
+echo "  _      _____     _______   _______  __   __  _____  _____   _____   ";
+echo " | |    /  _  \   /  _____/ /  _____/|  \ |  |/  _  \|  __ \ | ____|   ";
+echo " | |    | | | |  /  /_____ /  /_____ |   \|  || | | || |  | || |___    ";
+echo " | |    | | | | / _____  // _____  / |       || | | || |  | ||  ___|  ";
+echo " | |___ | |_| |  _____/ /  _____/ /  |  |\   || |_| || |__| || |___   ";
+echo " |_____|\_____//______ / /______ /   |__| \__|\_____/|_____/ |_____| ";
+echo -e "\e[0m"
+
+sleep 3
+
+if [ ! $NODENAME ]; then
+	read -p "Node adinizi girin: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m1. Sunucu guncellemesi yapiliyor.. \e[0m"
+echo "======================================================"
+sleep 1
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Gerekli kurulumlar yapiliyor.. \e[0m"
+echo "======================================================"
+sleep 1
+sudo apt install curl build-essential git wget jq make gcc tmux chrony -y
+
+echo -e "\e[1m\e[32m2. Go Yukleniyor.. \e[0m"
+echo "======================================================"
+sleep 1
+
+if ! [ -x "$(command -v go)" ]; then
+  cd
+  ver="1.18.5"
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+  rm "go$ver.linux-amd64.tar.gz"
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+  source $HOME/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Binary dosyalari yukleniyor.. \e[0m"
+echo "======================================================"
+sleep 1
+
+cd $HOME
+git clone https://github.com/okp4/okp4d.git
+cd okp4d
+make install
+
+okp4d version
+
+okp4d config chain-id okp4-nemeton
+okp4d config keyring-backend file
+okp4d config node tcp://localhost:26657
+okp4d init $NODENAME --chain-id okp4-nemeton
+
+
+echo -e "\e[1m\e[32m3. Genesis dosyasi indiriliyor, seed/peer ayari yapiliyor.. \e[0m"
+echo "======================================================"
+sleep 1
+
+wget -qO $HOME/.okp4d/config/genesis.json "https://raw.githubusercontent.com/okp4/networks/main/chains/nemeton/genesis.json"
+
+PEERS=f595a1386d5ca2e0d2cd81d3c6372c3bf84bbd16@65.109.31.114:2280,a49302f8999e5a953ebae431c4dde93479e17155@162.19.71.91:26656,dc14197ed45e84ca3afb5428eb04ea3097894d69@88.99.143.105:26656,79d179ea2e1fbdcc0c59a95ab7f1a0c48438a693@65.108.106.131:26706,501ad80236a5ac0d37aafa934c6ec69554ce7205@89.149.218.20:26656,5fbddca54548bf125ee96bb388610fe1206f087f@51.159.66.123:26656,769f74d3bb149216d0ab771d7767bd39585bc027@185.196.21.99:26656,024a57c0bb6d868186b6f627773bf427ec441ab5@65.108.2.41:36656,fff0a8c202befd9459ff93783a0e7756da305fe3@38.242.150.63:16656,2bfd405e8f0f176428e2127f98b5ec53164ae1f0@142.132.149.118:26656,bf5802cfd8688e84ac9a8358a090e99b5b769047@135.181.176.109:53656,dc9a10f2589dd9cb37918ba561e6280a3ba81b76@54.244.24.231:26656,085cf43f463fe477e6198da0108b0ab08c70c8ab@65.108.75.237:6040,803422dc38606dd62017d433e4cbbd65edd6089d@51.15.143.254:26656,b8330b2cb0b6d6d8751341753386afce9472bac7@89.163.208.12:26656
+
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.okp4d/config/config.toml
+
+
+okp4d tendermint unsafe-reset-all --home $HOME/.okp4d
+
+echo -e "\e[1m\e[32m4. Servis dosyasi olusturuluyor.. \e[0m"
+echo "======================================================"
+sleep 1
+
+sudo tee /etc/systemd/system/okp4d.service > /dev/null <<EOF
+[Unit]
+Description=okp4d
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which okp4d) start --home $HOME/.okp4d
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+echo -e "\e[1m\e[32m4. Servis baslatiliyor.. \e[0m"
+echo "======================================================"
+sleep 1
+
+sudo systemctl daemon-reload
+sudo systemctl enable okp4d
+sudo systemctl restart okp4d && sudo journalctl -u okp4d -f -o cat
